18f7bea0ff9c929043b7e522b4e2d1de9484ee32
ashutoshsharan774 <97532546+ashutoshsharan774@users.noreply.github.com>
2024-03-31 09:52:47 +0000

    conversion of datatypes

---
+++ 1_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+// let score = "33abc"
+// let score = true value 1 aayega
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// "33" gets converted to 33
+// "33abc" gets converted to NaN
+// true gets converted to 1 and false => 0
+
+// let isLoggedIn = 1
+let isLoggedIn =""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// empty string gets convrted to false in Boolean
+// string gets converted to true in Boolean
+
+
+let num =33
+let stringnum=String(num);
+console.log(stringnum);
+console.log(typeof stirngnum);
