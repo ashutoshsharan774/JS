8cc0689a3676141cbed2c9597b7a6cf6d4e113ba
ashutoshsharan774 <97532546+ashutoshsharan774@users.noreply.github.com>
2024-03-31 09:26:03 +0000

    Datatypes

---
+++ 1_basics/1_datatypes.js
@@ -0,0 +1,25 @@
+ "use strict"; //treat all JS code for newer version
+
+//  alert("hello") we are using node js , not browser
+
+console.log(3+3)  
+console.log("Ashutosh")
+// code readability should be given importance
+
+let name ="Ashu"
+let age= 19
+let loggedIn=false
+
+//number => 2^53 (range)
+// bigint
+// string (prefer in " ")
+// boolean (true/false)
+// null(standalone value) representation of empty
+// undefined (when value is not defined)
+// symbol (unique identifier , symbols are used to create object properties)
+
+// object
+
+console.log(typeof "Ashu")
+console.log(typeof undefined);
+// console.log(typeof null); null ka tupe object hai
