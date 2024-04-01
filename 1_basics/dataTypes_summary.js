5545f7515d3b7389df805f94a7734328a33df540
ashutoshsharan774 <97532546+ashutoshsharan774@users.noreply.github.com>
2024-04-01 06:07:50 +0000

    stack and heap memory

--- 1_basics/dataTypes_summary.js
+++ 1_basics/dataTypes_summary.js
@@ -38,3 +38,48 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+/*Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object*/
+
+
+
+    //    ******** Memory *******
+    // Stack(primitive). Heap(Non-primitive)
+
+    let myName="Ashutosh Sharan";
+
+    let anotherName=myName;
+    anotherName="Panda";
+
+    // Basically anotherName consists a copy of myName so any changes made in anotherName won't reflect in original myName
+    // So this is example of primitive (stack) data type
+
+    console.log(myName);
+    console.log(anotherName);
+
+    //object data type example
+    // The variable userOne will be in stack memory but the componenets or member of userOne i.e email and upiId will be stored in heap memory
+
+    let userOne={
+        email: "Ashu@gmail.com",
+        upiId: "Ashu@ybl"
+    }
+    let userTwo=userOne;
+    //userTwo won't have copy of components of userOne rather it would have direct reference to the actual location of 
+    // componenets/data members in heap
+    userTwo.email="ash@yahoo.com";//since actual reference was made so changes made reflects in actual location(object)
+    console.log(userOne.email)
+    console.log(userTwo.email);
+    // email changes in both the objects
