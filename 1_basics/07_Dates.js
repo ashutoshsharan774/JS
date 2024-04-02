72f7f8fae9906866a5fc096e3b4e832583a82c2d
ashutoshsharan774 <97532546+ashutoshsharan774@users.noreply.github.com>
2024-04-02 19:17:03 +0000

    Dates

--- 1_basics/07_Dates.js
+++ 1_basics/07_Dates.js
@@ -1,4 +1,5 @@
 // Dates
+// Date is to be considered as an object
 
 let myDate = new Date()
 // console.log(myDate.toString());
@@ -14,12 +15,13 @@ let myCreatedDate = new Date("01-14-2023")
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
+// console.log(myTimeStamp);//Date.now will tell time in milliseconds from midnight of 1970s
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
 console.log(newDate);
+console.log(newDate.toString());
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
 
