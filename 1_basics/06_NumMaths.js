8f3b9302f26b29b49b1dfc1a6cfb24d655583266
ashutoshsharan774 <97532546+ashutoshsharan774@users.noreply.github.com>
2024-04-02 10:36:23 +0000

    Number and Maths

--- 1_basics/06_NumMaths.js
+++ 1_basics/06_NumMaths.js
@@ -1,4 +1,4 @@
-const score=400;//automatically js understood that its number
+/*const score=400;//automatically js understood that its number
 //Explicitly we have to initialize score with number so we r gonna use object->number
 
 const balance=new Number(100);
@@ -15,3 +15,26 @@ console.log(balance.toFixed(2));
 // toFixed is used to give precisely number of decimals
 const num=23.695675;
 console.log(num.toPrecision(3));
+
+const score1=10000000;
+console.log(score1.toLocaleString('en-IN'));*/
+
+
+// ******MATHS********
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
